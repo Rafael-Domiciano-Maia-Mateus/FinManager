4e9ec5085907cb40f3e44ead78e286f7c7e2a11b
Rafael-Domiciano-Maia-Mateus <rmaia7985@gmail.com>
2026-01-09 22:37:03 -0300

    Ajusta DATABASE_URL para produção

--- FinManage/FinManage/settings.py
+++ FinManage/FinManage/settings.py
@@ -78,12 +78,19 @@ WSGI_APPLICATION = 'FinManage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-    )
-}
+DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
